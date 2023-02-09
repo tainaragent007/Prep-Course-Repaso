74b71abeea9272830e4abba414eaa71d017d42dc
tainaragent <tainaragent007@gmail.com>
2023-02-09 02:03:47 -0300

    terminado JSI y JSII

---
+++ 02-JS-I/homework/01Funciones.js
@@ -0,0 +1,33 @@
+// FUNCIONES
+	
+function miFuncion(){
+	//codigo
+}
+
+const otraFuncion(){
+	//codigo
+}
+
+const yOtra = () => {
+	//codigo
+}
+
+// Ejemplos 
+function presentar(nombre, edad){
+	console.log('Hola, mi nombre es ' + nombre);
+	console.log('y tengo ' + edad + ' anhos.');
+}
+
+// Function con parametros 
+function saludo(){
+	console.log('Hola, que tal?');
+}
+
+// Funciones - Argumentos
+function saluda(nombre){
+	console.log('Hola '+ nombre);
+}
+
+function suma(a, b){
+	console.log(a + b);
+}
