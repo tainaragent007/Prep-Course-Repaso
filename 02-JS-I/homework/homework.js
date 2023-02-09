3b39bb65453c192bd3acfbf4799ce18528836716
tainaragent <tainaragent007@gmail.com>
2023-02-08 22:49:02 -0300

    ya terminado J02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -186,7 +186,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return str + '!';
+  return `${str}!`
+  // return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
